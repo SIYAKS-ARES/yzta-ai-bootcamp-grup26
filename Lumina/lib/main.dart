c7f3e7750c43a2fe93c12d455f76b61ec4ffd991
ipekatmaca <ipekeylul02@gmail.com>
2025-07-16 23:54:20 +0300

    main.dart dosyası güncellendi.

--- Lumina/lib/main.dart
+++ Lumina/lib/main.dart
@@ -1,27 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:lumina/pages/auth_page.dart';
+import 'pages/home_page.dart';
 
 void main() {
-  runApp(const LuminaApp());
+  runApp(LuminaApp());
 }
 
 class LuminaApp extends StatelessWidget {
-  const LuminaApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Lumina',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.indigo,
-        fontFamily: 'Inter',
+        primarySwatch: Colors.indigo, // veya Colors.blue, sana kalmış
+        fontFamily: 'Inter', // veya 'Roboto', yine sen seç
         scaffoldBackgroundColor: Colors.white,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      initialRoute: '/',
+      initialRoute: '/', // ilk açılan sayfa AuthPage
       routes: {
         '/': (context) => AuthPage(),
+        '/home': (context) => HomePage(userName: 'Ahmet'), // userName parametresiyle örnek
       },
     );
   }
